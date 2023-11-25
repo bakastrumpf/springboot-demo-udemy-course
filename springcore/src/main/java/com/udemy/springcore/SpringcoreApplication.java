af5ccf280bb52af71a027125a604dc256a43eaaa
bakastrumpf <baka.strumpf@gmail.com>
2023-11-25 23:12:49 +0100

    fixed by scanBasePackages

--- springcore/src/main/java/com/udemy/springcore/SpringcoreApplication.java
+++ springcore/src/main/java/com/udemy/springcore/SpringcoreApplication.java
@@ -3,7 +3,10 @@ package com.udemy.springcore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {"com.Udemy.springcore",
+		"util"})
+
 public class SpringcoreApplication {
 
 	public static void main(String[] args) {
