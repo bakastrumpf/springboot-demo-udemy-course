9f544e2ef638842ac8e9820031169b85690290be
bakastrumpf <baka.strumpf@gmail.com>
2023-11-25 21:50:42 +0100

    injected custom properties via app.props file

--- test_app/src/main/java/com/spring_demo/test_app/rest/RestController.java
+++ test_app/src/main/java/com/spring_demo/test_app/rest/RestController.java
@@ -1,5 +1,6 @@
 package com.spring_demo.test_app.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.web.bind.annotation.RestController
@@ -22,5 +23,21 @@ public class RestController {
 	@GetMapping("/fortune")
 	public String fortuneTeller() {
 		return "Today is your lucky day!";
+	}
+	
+	// inject properties for: coach.name and team.name
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
+	@Value("${team.year}")
+	private String teamYear;
+	
+	// expose new "teaminfo" endpoint
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return coachName + " has just signed contract to coach " + teamName + " for the year " + teamYear + ".";
 	}
 }
