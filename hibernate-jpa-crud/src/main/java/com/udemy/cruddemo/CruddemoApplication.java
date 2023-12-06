12c81a5ad1087f99d6c43663838ba7efd6120cb5
bakastrumpf <baka.strumpf@gmail.com>
2023-12-06 21:48:49 +0100

    multiple students created

--- hibernate-jpa-crud/src/main/java/com/udemy/cruddemo/CruddemoApplication.java
+++ hibernate-jpa-crud/src/main/java/com/udemy/cruddemo/CruddemoApplication.java
@@ -19,23 +19,44 @@ public class CruddemoApplication {
 		public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 			
 			return runner -> {
-				//System.out.println("Hello, world!");
-				createStudent(studentDAO);
+				// System.out.println("Hello, world!");
+				// createStudent(studentDAO);
+				
+				createMultipleStudents(studentDAO);
 			};
 		}
+		
+		private void createMultipleStudents(StudentDAO studentDAO) {
+			
+			// create multiple students
+			
+			System.out.println("Creating 3 student objects... ");
+			Student tempStudent1 = new Student("Melissa", "Thestudent", "melissa@thestudent.net");
+			Student tempStudent2 = new Student("James", "Estudante", "james@estudante.net");
+			Student tempStudent3 = new Student("Janet", "Studente", "janet@studente.net");
+			
+			// save the student objects
+			
+			System.out.println("Saving the students... ");
+			studentDAO.save(tempStudent1);
+			studentDAO.save(tempStudent2);
+			studentDAO.save(tempStudent3);
+			
+		}
+	
 		
 		private void createStudent(StudentDAO studentDAO) {
 			
 			// create the student object
 			System.out.println("Creating new student object... ");
-			Student tempStudent = new Student("Paul", "Thestudent", "paul@thestudent.net");
+			Student tempStudent0 = new Student("Paul", "Thestudent", "paul@thestudent.net");
 			
 			// save the student object
 			System.out.println("Saving the student... ");
-			studentDAO.save(tempStudent);
+			studentDAO.save(tempStudent0);
 			
 			// display id of the saved student
-			System.out.println("Saved student. Generated id: " + tempStudent.getId());
+			System.out.println("Saved student. Generated id: " + tempStudent0.getId());
 		}
 
 }
