066b26e33358cbea6445b114ad758cc070535d5c
bakastrumpf <baka.strumpf@gmail.com>
2023-12-11 22:11:01 +0100

    edge cases exception handling | lesson 110

--- rest-apis/src/main/java/com/springframework/boot/restapis/rest/StudentRestController.java
+++ rest-apis/src/main/java/com/springframework/boot/restapis/rest/StudentRestController.java
@@ -60,17 +60,31 @@ public class StudentRestController {
 	}
 	
 	// add an exception handler using @ExceptionHandler
+//	@ExceptionHandler
+//	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+//		
+//		// create a StudentErrorResponse
+//		StudentErrorResponse error = new StudentErrorResponse();
+//		error.setStatus(HttpStatus.NOT_FOUND.value());
+//		error.setMessage(exc.getMessage());
+//		error.setTimeStamp(System.currentTimeMillis());
+//		
+//		// return ResponseEntity		
+//		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+//	}
+	
+	// add another exception handler for other edge cases
 	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+	public ResponseEntity<StudentErrorResponse> handleOtherExceptions(StudentNotFoundException exc){
 		
 		// create a StudentErrorResponse
 		StudentErrorResponse error = new StudentErrorResponse();
-		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
 		error.setMessage(exc.getMessage());
 		error.setTimeStamp(System.currentTimeMillis());
 		
 		// return ResponseEntity		
-		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
 	}
 
 }
