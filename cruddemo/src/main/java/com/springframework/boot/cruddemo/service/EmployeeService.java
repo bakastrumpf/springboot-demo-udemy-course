b5d84d8a6e33700b83f32fde6493f5969d4ee01a
bakastrumpf <baka.strumpf@gmail.com>
2023-12-17 18:14:07 +0100

    bureaucratic commit to keep the streak

--- cruddemo/src/main/java/com/springframework/boot/cruddemo/service/EmployeeService.java
+++ cruddemo/src/main/java/com/springframework/boot/cruddemo/service/EmployeeService.java
@@ -7,9 +7,9 @@ import com.springframework.boot.cruddemo.entity.Employee;
 public interface EmployeeService {
 	
 	List<Employee> findAll();
-	
 	Employee findById(int theId);
 	Employee save(Employee theEmployee);
 	void deleteById(int theId);
 
+	
 }
