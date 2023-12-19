62c1cf777a521af076303d2e7960bab79f765c21
bakastrumpf <baka.strumpf@gmail.com>
2023-12-19 21:23:17 +0100

    update employee | lesson 129

--- cruddemo/src/main/java/com/springframework/boot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/springframework/boot/cruddemo/rest/EmployeeRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,8 +63,14 @@ public class EmployeeRestController {
 		theEmployee.setId(0);
 		Employee dbEmployee = employeeService.save(theEmployee);
 		return dbEmployee;
-		
 	}
 	
+	
+	// add mapping for PUT /employees - update existing employee
+	@PutMapping("/employee")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
+		Employee dbEmployee = employeeService.save(theEmployee);
+		return dbEmployee;
+	}
 
 }
