2b86ca37861d3a710f5946c4cb246133939402aa
bakastrumpf <baka.strumpf@gmail.com>
2023-12-20 19:19:56 +0100

    crud with JPA | lesson 132

---
+++ cruddemo-w-jpa/src/main/java/com/springframework/boot/cruddemowjpa/CruddemoWJpaApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.boot.cruddemowjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddemoWJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoWJpaApplication.class, args);
+	}
+
+}
---
+++ cruddemo-w-jpa/src/test/java/com/springframework/boot/cruddemowjpa/CruddemoWJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.boot.cruddemowjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoWJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
