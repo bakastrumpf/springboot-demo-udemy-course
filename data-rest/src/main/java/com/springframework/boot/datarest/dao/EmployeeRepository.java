9a084f362660321b706a851c064472aa637716df
bakastrumpf <baka.strumpf@gmail.com>
2023-12-20 20:18:04 +0100

    changing pagination | lesson 138

--- data-rest/src/main/java/com/springframework/boot/datarest/dao/EmployeeRepository.java
+++ data-rest/src/main/java/com/springframework/boot/datarest/dao/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.springframework.boot.datarest.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@RepositoryRestResource(path="members")
 import com.springframework.boot.datarest.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
