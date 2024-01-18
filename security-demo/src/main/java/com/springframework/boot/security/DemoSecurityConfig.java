68469ca5b52141844e53201ecd9eaa3c2988b135
bakastrumpf <baka.strumpf@gmail.com>
2024-01-18 22:24:32 +0100

    implemented http requests and csrf | sth crashes | lesson 145

--- security-demo/src/main/java/com/springframework/boot/security/DemoSecurityConfig.java
+++ security-demo/src/main/java/com/springframework/boot/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.springframework.boot.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -12,31 +16,55 @@ public class DemoSecurityConfig {
 	@Bean
 	public InMemoryUserDetailsManager userDetailManager() {
 		
-		UserDetails john = User.builder()
-				.username("john")
+		UserDetails brenda = User.builder()
+				.username("brenda")
 				.password("{noop}test123")
 				.roles("EMPLOYEE")
 				.build();
 		
-		UserDetails mary = User.builder()
-				.username("mary")
+		UserDetails dylan = User.builder()
+				.username("dylan")
 				.password("{noop}test124")
 				.roles("EMPLOYEE")
 				.build();
 		
-		UserDetails wayne = User.builder()
-				.username("wayne")
+		UserDetails donna = User.builder()
+				.username("donna")
 				.password("{noop}test125")
 				.roles("EMPLOYEE","MANAGER")
 				.build();
 		
-		UserDetails susan = User.builder()
-				.username("susan")
+		UserDetails david = User.builder()
+				.username("david")
 				.password("{noop}test126")
 				.roles("EMPLOYEE","MANAGER", "ADMIN")
 				.build();
 		
-		return new InMemoryUserDetailsManager(john, mary, wayne, susan);
+		return new InMemoryUserDetailsManager(brenda, dylan, donna, david);
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests(configurer -> 
+			configurer
+				.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+				// ** is a wild card that will match all sub-paths
+				.requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+				// to update an existing employee:
+				.requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+			);
+		
+		// use http basic auth
+		http.httpBasic(Customizer.withDefaults());
+		
+		// disable csrf - Cross Site Request Forgery
+		// in general, not required for stateless REST APIs that use POST, PUT, DELETE
+		http.csrf(csrf -> csrf.disable());
+		
+		return http.build();
 		
 	}
 }
