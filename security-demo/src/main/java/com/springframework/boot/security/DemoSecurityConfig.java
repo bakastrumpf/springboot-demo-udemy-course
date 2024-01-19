5632c415cfeeb15c2312a00d2bd23a56bea36726
bakastrumpf <baka.strumpf@gmail.com>
2024-01-19 23:12:25 +0100

    fine tuning the DB queries in the code | lesson 157

--- security-demo/src/main/java/com/springframework/boot/security/DemoSecurityConfig.java
+++ security-demo/src/main/java/com/springframework/boot/security/DemoSecurityConfig.java
@@ -22,14 +22,20 @@ public class DemoSecurityConfig {
 	
 	@Bean
 	public UserDetailsManager userDetailsManager(DataSource dataSource) {
-		JdbcUserDetailsManager theUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 		
 		// "?" is the param to be used from the DB
 		// before using this method, run the sql script 6
-		theUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
-		theUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
 		
-		return new JdbcUserDetailsManager(dataSource);
+		// define query to retrieve a user by username
+		jdbcUserDetailsManager.setUsersByUsernameQuery(
+				"select user_id, pw, active from members where user_id=?");
+		
+		// define query to retrieve the authorities / roles by username
+		jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+				"select user_id, role from roles where user_id=?");
+		
+		return jdbcUserDetailsManager;
 	}
 	
 	@Bean
