48f56b8213f75cb80c0ee32e1743c5006febf784
bakastrumpf <baka.strumpf@gmail.com>
2024-01-22 20:49:34 +0100

    second attempt of thymeleaf project

--- 09-thymeleaf-demo/src/main/java/com/springframework/boot/thymeleafdemo/ThymeleafDemoApplication.java
+++ 09-thymeleaf-demo/src/main/java/com/springframework/boot/thymeleafdemo/ThymeleafDemoApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.demo;
+package com.springframework.boot.thymeleafdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
