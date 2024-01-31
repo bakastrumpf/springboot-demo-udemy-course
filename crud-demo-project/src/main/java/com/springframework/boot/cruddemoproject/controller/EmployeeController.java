45a13896ef55fe54314f13988b2d488305a908b1
baka.strumpf <baka.strumpf@gmail.com>
2024-01-31 16:24:27 +0100

    new old project setup - does not work yet | lesson 216

---
+++ crud-demo-project/src/main/java/com/springframework/boot/cruddemoproject/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package com.springframework.boot.cruddemoproject.controller;
+
+import com.springframework.boot.cruddemoproject.entity.Employee;
+import com.springframework.boot.cruddemoproject.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService theEmployeeService){
+        employeeService = theEmployeeService;
+    }
+
+        @GetMapping("/list")
+        // add mapping for "/list"
+        public String listEmployees(Model theModel){
+
+            // get employees from db
+            List<Employee> theEmployees = employeeService.findAll();
+
+            // add to the spring model
+            theModel.addAttribute("employees", theEmployees);
+
+            return "list-employees";
+        }
+
+}
