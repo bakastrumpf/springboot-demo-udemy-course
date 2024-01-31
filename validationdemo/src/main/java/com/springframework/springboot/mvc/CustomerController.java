d44edf1eb5f90c2d2146eda4c7c3ccf2b369a5b6
baka.strumpf <baka.strumpf@gmail.com>
2024-01-31 13:28:44 +0100

    debugging trick added to inspect binding results | lesson 206

--- validationdemo/src/main/java/com/springframework/springboot/mvc/CustomerController.java
+++ validationdemo/src/main/java/com/springframework/springboot/mvc/CustomerController.java
@@ -36,7 +36,20 @@ public class CustomerController {
             @Valid @ModelAttribute("customer") Customer theCustomer,
             BindingResult theBindingResult) {
 
+        // debugging trick:
         System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+        System.out.println("Binding results: |" + theBindingResult.toString() + "|");
+        System.out.println("\n\n\n\n\n");
+
+        /*
+        renders the following:
+
+        Last name: |null|
+        Binding results: |org.springframework.validation.BeanPropertyBindingResult: 3 errors
+        Field error in object 'customer' on field 'freePasses': rejected value [hgj9652]; codes [typeMismatch.customer.freePasses,typeMismatch.freePasses,typeMismatch.java.lang.Integer,typeMismatch]; arguments [org.springframework.context.support.DefaultMessageSourceResolvable: codes [customer.freePasses,freePasses]; arguments []; default message [freePasses]]; default message [Failed to convert property value of type 'java.lang.String' to required type 'java.lang.Integer' for property 'freePasses'; For input string: "hgj9652"]
+        Field error in object 'customer' on field 'postalCode': rejected value [gg551]; codes [Pattern.customer.postalCode,Pattern.postalCode,Pattern.java.lang.String,Pattern]; arguments [org.springframework.context.support.DefaultMessageSourceResolvable: codes [customer.postalCode,postalCode]; arguments []; default message [postalCode],[Ljakarta.validation.constraints.Pattern$Flag;@395f0883,^[0-9]{5}]; default message [must contain 5 digits]
+        Field error in object 'customer' on field 'lastName': rejected value [null]; codes [NotNull.customer.lastName,NotNull.lastName,NotNull.java.lang.String,NotNull]; arguments [org.springframework.context.support.DefaultMessageSourceResolvable: codes [customer.lastName,lastName]; arguments []; default message [lastName]]; default message [is required]|
+         */
         if(theBindingResult.hasErrors()){
             return "customer-form";
         }
