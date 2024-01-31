d8b5a31b01f715a8e6427ea9777c658f227870f8
baka.strumpf <baka.strumpf@gmail.com>
2024-01-31 21:34:00 +0100

    index.html added | lesson 219 | NOT WORKING YET

--- crud-demo-project/src/main/java/com/springframework/boot/cruddemoproject/controller/EmployeeController.java
+++ crud-demo-project/src/main/java/com/springframework/boot/cruddemoproject/controller/EmployeeController.java
@@ -2,9 +2,7 @@ package com.springframework.boot.cruddemoproject.controller;
 
 import com.springframework.boot.cruddemoproject.entity.Employee;
 import com.springframework.boot.cruddemoproject.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
