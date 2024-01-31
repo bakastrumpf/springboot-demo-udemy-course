f843d089ba4d587c853af5395ce979643adb9a6b
baka.strumpf <baka.strumpf@gmail.com>
2024-01-31 20:40:33 +0100

    employees list functional | lesson 217

--- crud-demo-project/src/main/java/com/springframework/boot/cruddemoproject/controller/EmployeeController.java
+++ crud-demo-project/src/main/java/com/springframework/boot/cruddemoproject/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.springframework.boot.cruddemoproject.entity.Employee;
 import com.springframework.boot.cruddemoproject.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,17 +20,17 @@ public class EmployeeController {
         employeeService = theEmployeeService;
     }
 
-        @GetMapping("/list")
-        // add mapping for "/list"
-        public String listEmployees(Model theModel){
+    @GetMapping("/list")
+    // add mapping for "/list"
+    public String listEmployees(Model theModel){
 
-            // get employees from db
-            List<Employee> theEmployees = employeeService.findAll();
+        // get employees from db
+        List<Employee> theEmployees = employeeService.findAll();
 
-            // add to the spring model
-            theModel.addAttribute("employees", theEmployees);
+        // add to the spring model
+        theModel.addAttribute("employees", theEmployees);
 
-            return "list-employees";
-        }
+        return "list-employees";
+    }
 
 }
--- crud-demo-project/src/main/java/com/springframework/boot/cruddemoproject/service/EmployeeServiceImpl.java
+++ crud-demo-project/src/main/java/com/springframework/boot/cruddemoproject/service/EmployeeServiceImpl.java
@@ -3,10 +3,12 @@ package com.springframework.boot.cruddemoproject.service;
 import com.springframework.boot.cruddemoproject.dao.EmployeeRepository;
 import com.springframework.boot.cruddemoproject.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
