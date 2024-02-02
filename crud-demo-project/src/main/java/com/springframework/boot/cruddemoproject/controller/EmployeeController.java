2f11c912d8ce7ce653e9d96d9ab1c560c2a5ba0e
baka.strumpf <baka.strumpf@gmail.com>
2024-02-02 15:46:09 +0100

    add & save new employee, display order by last name | lessons 220-223

--- crud-demo-project/src/main/java/com/springframework/boot/cruddemoproject/controller/EmployeeController.java
+++ crud-demo-project/src/main/java/com/springframework/boot/cruddemoproject/controller/EmployeeController.java
@@ -18,6 +18,28 @@ public class EmployeeController {
         employeeService = theEmployeeService;
     }
 
+    @GetMapping("/showAddForm")
+    public String showAddForm(Model theModel){
+
+        // create model attribute to bind form data
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+
+        // save the employee
+        employeeService.save(theEmployee);
+
+        // use a redirect to prevent duplicate submissions
+        // implementation of "Post/Redirect/Get" pattern
+        return "redirect:/employees/list";
+    }
+
     // add mapping for "/list"
     @GetMapping("/list")
     public String listEmployees(Model theModel){
@@ -28,7 +50,7 @@ public class EmployeeController {
         // add to the spring model
         theModel.addAttribute("employees", theEmployees);
 
-        return "list-employees";
+        return "employees/list-employees";
     }
 
 }
--- crud-demo-project/src/main/java/com/springframework/boot/cruddemoproject/dao/EmployeeRepository.java
+++ crud-demo-project/src/main/java/com/springframework/boot/cruddemoproject/dao/EmployeeRepository.java
@@ -3,9 +3,15 @@ package com.springframework.boot.cruddemoproject.dao;
 import com.springframework.boot.cruddemoproject.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-    public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
         // no need to write any other code
         // the beauty of Spring data jpa
+
+        // sort by last name
+
+        public List<Employee> findAllByOrderByLastNameAsc();
 
     }
