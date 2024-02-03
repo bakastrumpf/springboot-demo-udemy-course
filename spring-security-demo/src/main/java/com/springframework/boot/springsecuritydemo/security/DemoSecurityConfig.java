fb50bdbcbdb965ef0ca394ddbe746dda1ddee6ee
baka.strumpf <baka.strumpf@gmail.com>
2024-02-03 20:41:11 +0100

    custom login page config | lesson 237

--- spring-security-demo/src/main/java/com/springframework/boot/springsecuritydemo/security/DemoSecurityConfig.java
+++ spring-security-demo/src/main/java/com/springframework/boot/springsecuritydemo/security/DemoSecurityConfig.java
@@ -2,10 +2,12 @@ package com.springframework.boot.springsecuritydemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -39,5 +41,21 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(brenda, dylan, donna, david);
 
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .anyRequest().authenticated()
+        )
+                .formLogin(form ->
+                        form
+                                .loginPage("showLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                );
+
+        return http.build();
     }
 }
