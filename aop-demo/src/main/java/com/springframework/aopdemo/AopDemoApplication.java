abdd990477daf52b6a0703868f3c7c47dac8c79b
baka.strumpf <baka.strumpf@gmail.com>
2024-02-10 07:27:21 +0100

    AOP demo project setup | lesson 340

---
+++ aop-demo/src/main/java/com/springframework/aopdemo/AopDemoApplication.java
@@ -0,0 +1,23 @@
+package com.springframework.aopdemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopDemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+
+		return runner -> {
+			System.out.println("Hello, world!");
+		};
+	}
+
+}
