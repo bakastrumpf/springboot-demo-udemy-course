fc80cd4ae697e43dc0e103af31b74569d7359996
baka.strumpf <baka.strumpf@gmail.com>
2024-02-13 16:10:11 +0100

    match a single method with @Before | lesson 348

--- aop-demo/src/main/java/com/springframework/aopdemo/aspect/DemoLoggingAspect.java
+++ aop-demo/src/main/java/com/springframework/aopdemo/aspect/DemoLoggingAspect.java
@@ -13,7 +13,7 @@ public class DemoLoggingAspect {
     // this is where we all of our related advice for logging
 
     // starting with @Before advice
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void com.springframework.aopdemo.DAO.AccountDAO.addAccount())")
     // @Before("execution(public void updateAccount())")
     public void beforeAddAccountAdvice(){
         System.out.println("\n =====>>> Executing @Before advice on method");
