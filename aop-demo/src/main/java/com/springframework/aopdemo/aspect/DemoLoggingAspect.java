ecdf73767a151718484e1f6210bac57ae565754d
baka.strumpf <baka.strumpf@gmail.com>
2024-02-13 19:00:44 +0100

    pointcut declarations | lesson 357

--- aop-demo/src/main/java/com/springframework/aopdemo/aspect/DemoLoggingAspect.java
+++ aop-demo/src/main/java/com/springframework/aopdemo/aspect/DemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.springframework.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -13,13 +14,23 @@ public class DemoLoggingAspect {
     // this is where we all of our related advice for logging
 
     // starting with @Before advice
+
+    @Pointcut("execution(* com.springframework.aopdemo.DAO.*.*())")
+    private void forDaoPackage(){}
+
+    @Before("forDaoPackage()")
+    public void performApiAnalytics(){
+
+    }
+
     // @Before("execution(public void com.springframework.aopdemo.DAO.AccountDAO.addAccount())")
     // @Before("execution(public void updateAccount())")
     // @Before("execution(public void add*())")
     // @Before("execution(* add*(Account))")
     // @Before("execution(* add*(com.springframework.aopdemo.Account))")
     // @Before("execution(* add*(com.springframework.aopdemo.Account, ..))")
-    @Before("execution(* com.springframework.aopdemo.DAO.*.*(..))")
+    // @Before("execution(* com.springframework.aopdemo.DAO.*.*(..))")
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice(){
         System.out.println("\n =====>>> Executing @Before advice on method");
     }
