cef133b3f6c76052688eb56cb66e55c933765ec6
baka.strumpf <baka.strumpf@gmail.com>
2024-02-15 20:39:58 +0100

    update main app | lesson 373

--- aop-demo/src/main/java/com/springframework/aopdemo/AopDemoApplication.java
+++ aop-demo/src/main/java/com/springframework/aopdemo/AopDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class AopDemoApplication {
 
@@ -20,8 +22,21 @@ public class AopDemoApplication {
 
 		return runner -> {
 			// System.out.println("Hello, world!");
-			demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
+			// demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
+			demoTheAfterReturningAdvice(theAccountDAO);
 		};
+	}
+
+	private void demoTheAfterReturningAdvice(AccountDAO theAccountDAO) {
+
+		// call method to find the accounts
+		List<Account> theAccounts = theAccountDAO.findAccounts();
+
+		// display the accounts
+		System.out.println("\n\nMain Program: demoTheAfterReturningAdvice");
+		System.out.println("-----");
+		System.out.println(theAccounts);
+		System.out.println("\n");
 	}
 
 	private void demoTheBeforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
