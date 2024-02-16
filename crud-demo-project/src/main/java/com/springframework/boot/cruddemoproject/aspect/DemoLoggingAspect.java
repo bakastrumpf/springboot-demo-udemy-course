f87e9ed1c153a84ff2ae977f751a4e9f1ba6e13f
baka.strumpf <baka.strumpf@gmail.com>
2024-02-16 20:38:25 +0100

    add aspect to mvc spring thymeleaf | lessons 393

--- crud-demo-project/src/main/java/com/springframework/boot/cruddemoproject/aspect/DemoLoggingAspect.java
+++ crud-demo-project/src/main/java/com/springframework/boot/cruddemoproject/aspect/DemoLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.springframework.boot.cruddemoproject.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -11,4 +12,17 @@ public class DemoLoggingAspect {
 
     // setup logger
     private Logger myLogger = Logger.getLogger(getClass().getName());
+
+    // setup pointcut declarations
+    @Pointcut("execution(* com.springframework.boot.cruddemoproject.controller.*.*(..))")
+    private void forControllerPackage() {}
+
+    // setup pointcut declarations for service
+    @Pointcut("execution(* com.springframework.boot.cruddemoproject.service.*.*(..))")
+    private void forServicePackage() {}
+
+    // setup pointcut declarations for dao
+    @Pointcut("execution(* com.springframework.boot.cruddemoproject.dao.*.*(..))")
+    private void forDaoPackage() {}
+
 }
