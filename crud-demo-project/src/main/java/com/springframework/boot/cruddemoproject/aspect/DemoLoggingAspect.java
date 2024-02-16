fcaab51ce8f86c189e4d5d85434e12ef8f341a03
baka.strumpf <baka.strumpf@gmail.com>
2024-02-16 20:49:01 +0100

    add logging | lessons 394

--- crud-demo-project/src/main/java/com/springframework/boot/cruddemoproject/aspect/DemoLoggingAspect.java
+++ crud-demo-project/src/main/java/com/springframework/boot/cruddemoproject/aspect/DemoLoggingAspect.java
@@ -39,6 +39,15 @@ public class DemoLoggingAspect {
         myLogger.info("=====>>>>> in @Before: calling method: " + theMethod);
 
         // display the arguments to the method
+
+
+        // get the arguments
+        Object[] args = theJoinPoint.getArgs();
+
+        // loop through and display the args
+        for (Object tempArg : args) {
+            myLogger.info("=====>>>>> argument: " + tempArg);
+        }
     }
 
 }
